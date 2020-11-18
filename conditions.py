5da533c364d4aeefab7431f72503d95dab44e5b4
mdohoney <35897938+mdohoney@users.noreply.github.com>
2020-11-18 13:12:40 -0600

    Create conditions.py

---
+++ conditions.py
@@ -0,0 +1,11 @@
+age = int(input("How old are you? "))
+
+if 16 <= age <= 65:
+    print("Have a good day at work...")
+else:
+    print("Enjoy your free time...")
+print("_" * 100)
+if age < 16 or age > 65:
+    print("Enjoy your free time.")
+else:
+    print("Have a good day at work.")
