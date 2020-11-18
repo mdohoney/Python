35ba61a5054aaf747b2adaf1b5dab950b9a3106b
mdohoney <35897938+mdohoney@users.noreply.github.com>
2020-11-18 13:14:24 -0600

    Create multipleassign.py

---
+++ multipleassign.py
@@ -0,0 +1,26 @@
+#Declar multiple variables, 
+#Assign a single value to them,
+#And print them.
+
+t=p=q=1024
+print('The value of t is:' ,t)
+print('The value of p is:' ,p)
+print('The value of q is:' ,q)
+
+print()
+print("*" * 100)
+print()
+
+j,u,p,i,t,e,r = "Big" , 1344,512.57, "planet" ,90, 87, 66
+print('The value of j is :' , j)
+print('The value of u is:' , u)
+print('The value of p is:' , p)
+print('The vlue of i is:', i)
+print('The value of t is:' , t)
+print('The value of e is:' , e)
+print('The value of r is:', r) 
+
+print()
+print("*" * 100)
+print()
+
