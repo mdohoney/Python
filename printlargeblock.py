0b310329dedb833681c92fa7743c82058577fd0a
mdohoney <35897938+mdohoney@users.noreply.github.com>
2020-11-18 13:16:29 -0600

    Create printlargeblock.py

---
+++ printlargeblock.py
@@ -0,0 +1,20 @@
+key_terms = [""" 
+Perceptual: acuity the ability to sense what is coming before the fog clears.\n 
+Environmental scanning: surveillance of a firm’s external environment to predict environmental changes and\n detect changes already under way.\n 
+Environmental monitoring: a firm’s analysis of the external environment that tracks the evolution of environmental trends,\n sequences of events, or streams of activities.\n 
+Competitive intelligence: a firm’s activities of collecting and interpreting data on competitors,\n defining and understanding the industry, and identifying competitors’ strengths and weaknesses. \n 
+Environmental forcasting: the development of plausible projections about the direction,\n scope, speed, and intensity of environmental change.\n 
+Scenario Analysis: an in-depth approach to environmental forecasting that involves experts’\n detailed assessments of societal trends, economics, politics, technology, or other dimensions of the external environment.\n
+SWOT analysis: a framework for analyzing a company’s internal and external environments and\n that stands for strengths, weaknesses, opportunities, and threats.\n
+General environment: factors external to an industry,\n and usually beyond a firm’s control, that affect a firm’s strategy.\n
+Demographic Segment of the general envrionment: genetic and observable characteristics of a population,\n including the levels and growth of age, density, sex, race, ethnicity, education, geographic region, and income.\n
+Sociocultural segment of the general environment: the values, beliefs, and lifestyles of a society.\n
+Political/legal segment of the gernal envronment:how a society creates and exercises power, including rules, laws, and taxation policies.\n
+\n""" ]
+
+for key_terms in key_terms:
+    print(f"Key Terms Week 1:\n Chapter 2:\n  {key_terms}.\n")
+
+from datetime import datetime
+print("Current date & time -->\t", datetime.now())
+
